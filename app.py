d34f939b0553ddda19acb89eb330a5774e3a4211
awerner5413 <werner.alex2@gmail.com>
2022-11-07 09:46:38 -0600

    added login page and updated style

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # I need to figure out how to handle all of the things that cs50 built in for me withi helpers
 # I believe that included the login validations, something to do with formatting USD, etc.
 # TD List
-# Add in the styles sheet from finance and adjust
-# Get the layout html file the way I want so I can build off it
 # see if we were using JS for anything in the finance project
+# Figure out how to use Postgresql and add a database for the login
+# Figure out how to authenticate and use the login with werkzungaifenf (see finance-site and helpers)
 
 from flask import Flask, render_template
 
@@ -12,3 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def launch_homepage():    
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():    
+    return render_template("login.html")
