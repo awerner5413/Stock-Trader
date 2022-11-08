efd0aeb62de642ff2d85ebb3843c892db741235f
awerner5413 <werner.alex2@gmail.com>
2022-11-08 13:44:38 -0600

    updates

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # I need to figure out how to handle all of the things that cs50 built in for me withi helpers
 # I believe that included the login validations, something to do with formatting USD, etc.
 # TD List
-# see if we were using JS for anything in the finance project
 # Figure out how to authenticate and use the login with werkzungaifenf (see finance-site and helpers)
-# Figure out how to use Postgresql and add a database for the login
+# Figure out how to use Postgresql and add a database for the login (finish backend section on ZtM)
 
 
 from flask import Flask, render_template
