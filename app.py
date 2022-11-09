b5a7fac3c7d64f8c2a5cdbbb75ce3360ba1cebba
awerner5413 <werner.alex2@gmail.com>
2022-11-09 11:41:38 -0600

    updates and adding reigstration page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # I need to figure out how to handle all of the things that cs50 built in for me withi helpers
 # I believe that included the login validations, something to do with formatting USD, etc.
-# TD List
+# -- TD List
+# Build registration page, connect to DB, build index/homepage, setup security measures and add standing login check
 # Figure out how to authenticate and use the login with werkzungaifenf (see finance-site and helpers)
-# Figure out how to use Postgresql and add a database for the login (finish backend section on ZtM)
 # Will need to use Flask request feature to bring in API - documentation has a section
 # User PythonAnywhere when it's time to release to production, they will host the server
-# Features to add
+# -- Features to add
 # A script that scrapes a stock ticker for news and displays first 3 articles below
 # Graph user cash total over time or per transaction
 
@@ -22,14 +22,18 @@ cursor = db.cursor()
 def launch_homepage():
 
     # TESTING SQL
-    sql = "INSERT INTO users2 (name, age, birthday, score) VALUES (%s, %s, %s, %s)"
-    val = ("Ben", 31, '1990-12-26', 42)
-    cursor.execute(sql, val)
-    db.commit()
-    print(cursor.rowcount, "record inserted.")    
+    # sql = "INSERT INTO users2 (name, age, birthday, score) VALUES (%s, %s, %s, %s)"
+    # val = ("Ben", 31, '1990-12-26', 42)
+    # cursor.execute(sql, val)
+    # db.commit()
+    # print(cursor.rowcount, "record inserted.")    
 
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():    
     return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():    
+    return render_template("register.html")
