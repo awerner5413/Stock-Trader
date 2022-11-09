319dc2292aecf32b2a04e45af5f9c09c184d8513
awerner5413 <werner.alex2@gmail.com>
2022-11-09 10:10:44 -0600

    added connection to postgreSQL db and ran a test

--- app.py
+++ app.py
@@ -3,14 +3,31 @@
 # TD List
 # Figure out how to authenticate and use the login with werkzungaifenf (see finance-site and helpers)
 # Figure out how to use Postgresql and add a database for the login (finish backend section on ZtM)
+# Will need to use Flask request feature to bring in API - documentation has a section
+# User PythonAnywhere when it's time to release to production, they will host the server
+# Features to add
+# A script that scrapes a stock ticker for news and displays first 3 articles below
+# Graph user cash total over time or per transaction
 
-
+import psycopg2
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+db = psycopg2.connect( database="test", user='postgres', password='Atg112523!', host='127.0.0.1', port='5432')
+db.autocommit = True
+cursor = db.cursor()
+
 @app.route('/')
-def launch_homepage():    
+def launch_homepage():
+
+    # TESTING SQL
+    sql = "INSERT INTO users2 (name, age, birthday, score) VALUES (%s, %s, %s, %s)"
+    val = ("Ben", 31, '1990-12-26', 42)
+    cursor.execute(sql, val)
+    db.commit()
+    print(cursor.rowcount, "record inserted.")    
+
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
