4502700ff49ef3236a46f1db4296db24b01731a7
awerner5413 <werner.alex2@gmail.com>
2022-11-10 00:51:27 -0600

    adding code for login and register, needs debugged

--- app.py
+++ app.py
@@ -9,21 +9,96 @@
 # A script that scrapes a stock ticker for news and displays first 3 articles below
 # Graph user cash total over time or per transaction
 
-from flask import Flask, render_template
-from authentication import require_login, get_cursor
+from flask import Flask, render_template, request, flash, session, redirect
+from authentication import get_db, get_cursor, require_login
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 @require_login
 def launch_homepage():
+    if request.method == "POST":
+        return render_template("index.html")
 
-    # TESTING SQL
-    # sql = "INSERT INTO users2 (name, age, birthday, score) VALUES (%s, %s, %s, %s)"
-    # val = ("Ben", 31, '1990-12-26', 42)
-    # cursor.execute(sql, val)
-    # db.commit()
-    # print(cursor.rowcount, "record inserted.")    
+@app.route("/register", methods=["GET", "POST"])
+def register():
 
-    return render_template("index.html")
+    # Get all variables from registration
+    if request.method == "POST":
+        # Get variables from registration form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hashed = generate_password_hash(password, method='pbkdf2:sha256', salt_length=len(password))
+        confirmation = request.form.get("confirmation")
+        error = None
+
+        # Setup SQL for db addition
+        db = get_db()
+        cursor = get_cursor()
+        sql = "INSERT INTO login (secret, name) VALUES (%s, %s)"
+        val = (hashed, username)
+        
+        # Confirm variables meet criteria
+        if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirmation"):
+            error = "You must enter a username and a password."
+        elif password != confirmation:
+            error = "Your password and confirmation must match."
+
+        # Hash the password and add user to users - FIGURE OUT HOW TO HASH
+        if error is None:            
+            try:
+                cursor.execute(sql, val)
+                db.commit()
+            except db.IntegrityError:
+                error = "Invalid username or password."
+            return render_template("login.html")
+        
+        flash(error)
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "POST":
+        error = None
+        cursor = get_cursor()
+        sql = "SELECT * FROM users2 WHERE name = %s"
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Ensure username and password was submitted
+        if not username or not password:
+            error = "You must provider a username and a password."
+            flash(error)
+            return render_template("login.html")
+    
+        # Query database for username
+        cursor.execute(sql, (username,))
+        rows = cursor.fetchall()
+        print(rows, "asldkjfalksdjf")
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["secret"], request.form.get("password")):
+            error = "Invalid password."
+        elif rows[0]["name"] != request.form.get("username"):
+            error = "Invalid username."
+
+        # Remember which user has logged in
+        else:
+            session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+            return redirect("/")
+
+        flash(error)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
--- authentication.py
+++ authentication.py
@@ -1,12 +1,17 @@
 # This is where I will handle my login checks and maybe some other stuff, like helper, but not just like it...
 import psycopg2
 from functools import wraps
-from flask import session, redirect, request
+from flask import session, redirect #, request, render_template, flash
+# from werkzeug.security import check_password_hash, generate_password_hash
 
 
-def get_cursor():
+def get_db():
     db = psycopg2.connect(database="test", user='postgres', password='Atg112523!', host='127.0.0.1', port='5432')
     db.autocommit = True
+    return db
+
+def get_cursor():    
+    db = get_db()
     cursor = db.cursor()
     return cursor
 
@@ -19,78 +24,82 @@ def require_login(func):
     return wrapper_func
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-    # Get all variables from registration
-    if request.method == "POST":
-        # Get variables from registration form
-        username = request.form.get("username")
-        password = request.form.get("password")
-        # confirmation = request.form.get("confirmation")
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
 
-        # Setup SQL for db addition
-        sql = "INSERT INTO login (secret, name) VALUES (%s, %s)"
-        val = (username, password)
+#     # Get all variables from registration
+#     if request.method == "POST":
+#         # Get variables from registration form
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=len(password))
+#         confirmation = request.form.get("confirmation")
+#         error = None
 
-        # NEED TO FIGURE OUT HOW TO HANDLE ERRORS DIFFERENT THAN WHAT THEY DID - POP UP ON SCREEN?
-        # Confirm variables meet criteria
-        # if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirmation"):
-        #     return apology("must enter a username or password", 400)
-        # elif password != confirmation:
-        #     return apology("password and confirmation must match", 400)
-
-        # Hash the password and add user to users - FIGURE OUT HOW TO HASH
-        # else:
-        #     # hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=len(password))
-        #     try:
-        #         db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
-        #     except ValueError as err: # FIGURE OUT HOW TO HANDLE ERROR HERE RATHER THAN RE-LOAD PAGE
-        #         return render_template("register.html")
-        #         # return apology("invalid username and/or password", 400)
-        #     return render_template("login.html")
+#         # Setup SQL for db addition
+#         db = get_db()
+#         cursor = get_cursor()
+#         sql = "INSERT INTO login (secret, name) VALUES (%s, %s)"
+#         val = (hash, username)
         
-        try:
-            print(username, password)
-            cursor.execute(sql, val)
-            db.commit()
-        except ValueError as err: # FIGURE OUT HOW TO HANDLE ERROR HERE RATHER THAN RE-LOAD PAGE, or do but with flash message
-            return render_template("register.html")            
-        return render_template("login.html")
+#         # Confirm variables meet criteria
+#         if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirmation"):
+#             error = "You must enter a username and a password."
+#         elif password != confirmation:
+#             error = "Your password and confirmation must match."
+
+#         # Hash the password and add user to users - FIGURE OUT HOW TO HASH
+#         if error is None:            
+#             try:
+#                 cursor.execute(sql, val)
+#                 db.commit()
+#             except db.IntegrityError:
+#                 error = "Invalid username or password."
+#             return render_template("login.html")
+        
+#         flash(error)
+
+#     else:
+#         return render_template("register.html")
 
 
-    else:
-        return render_template("register.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    # Forget any user_id
-    session.clear()
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     # Forget any user_id
+#     session.clear()
     
-    if request.method == "POST":
+#     if request.method == "POST":
+#         error = None
+#         cursor = get_cursor()        
+#         sql = "SELECT * FROM users WHERE username = %s"
+#         un = request.form.get("username")
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            error = "You must provider username."
+#         # Ensure username was submitted
+#         if not request.form.get("username"):
+#             error = "You must provider username."
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            error = "You must provider password."
+#         # Ensure password was submitted
+#         elif not request.form.get("password"):
+#             error = "You must provider password."
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+#         # Query database for username
+#         rows = cursor.execute(sql, un)
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+#         # Ensure username exists and password is correct
+#         if len(rows) != 1 or not check_password_hash(rows[0]["secret"], request.form.get("password")):
+#             error = "Invalid password."
+#         elif rows[0]["name"] != request.form.get("username"):
+#             error = "Invalid username."
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+#         # Remember which user has logged in
+#         else:
+#             session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
-        return redirect("/")
+#         # Redirect user to home page
+#             return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
+#         flash(error)
+
+#     # User reached route via GET (as by clicking a link or via redirect)
+#     else:
+#         return render_template("login.html")
